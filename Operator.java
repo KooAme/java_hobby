00e73880a6634a6f5384f5fb42c462db0331e04a
KooAme <dg.changeworld@gmail.com>
2025-05-14 23:42:19 +0900

    test

---
+++ Operator.java
@@ -0,0 +1,11 @@
+public class Operator {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+        System.out.println(sum);
+        // - / % *
+        String result1 = "hello " + "world";
+        System.out.println(result1);
+    }
+}
